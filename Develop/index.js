9113008145809637414d3b05bf611aba3f403153
Kevin <khampkevin@gmail.com>
2023-04-28 22:03:03 -0400

    it works...kinda

--- Develop/index.js
+++ Develop/index.js
@@ -1,31 +1,29 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
 const generateMarkdown = require('../Develop/utils/generateMarkdown')
-// TODO: Create an array of questions for user input
+
 const questions = [
     {type: 'input', message: 'What is the title of this README?', name: 'title'},
     {type: 'input', message: 'What is the description of this README?', name: 'description'},
-    {type: 'input', message: 'What is the table of contents of this README?', name: 'table'},
-    {type: 'input', message: 'What is the installation?', name: 'installation'},
+    {type: 'input', message: 'How to install?', name: 'installation'},
     {type: 'input', message: 'What was used?', name: 'usage'},
-    {type: 'checkbox', message: 'What is the license?', name: 'license', choices: ['MIT', 'APACHE2.0', 'Boost1.0', 'BSD2', 'BSD3', 'NONE']},
+    {type: 'checkbox', message: 'What is the license?', name: 'license', choices: ['MIT', 'APACHE2.0', 'GPL', 'BSD', 'NONE']},
     {type: 'input', message: 'Who are you contributing?', name: 'contribution'},
     {type: 'input', message: 'How do I test this?', name: 'tests'},
     {type: 'input', message: 'What is your email?', name: 'email'},
     {type: 'input', message: 'What is your GitHub username?',name: 'github'},
 ];
 
-// TODO: Create a function to write README file
 function writeToFile(filename, data) {
-    return fs.writeFileSync('../Develop/', filename, data)
+    return fs.writeFile(filename, data, (err) =>
+    err ? console.log(err) : console.log('Writing file.'))
 }
 
-// TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((responses) => {
         console.log('Creating README.md...')
-        writeToFile('../Develop/README.md', generateMarkdown({...responses}))
+        writeToFile('README.md', JSON.stringify(generateMarkdown({...responses}),null), (err) =>
+        err ? console.error(err) : console.log(''))
     })
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,33 +1,30 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+
 function renderLicenseBadge(license) {
-  if (license !== 'none') {
-  return `![Github license](https://img.shields.io/badge/license-${license}-yellowgreen.svg`
+  if (license !== 'NONE') {
+  return `![Github license](https://img.shields.io/badge/license-${license}-yellowgreen.svg`;
   }
-  return ''
+  return '';
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
+
 function renderLicenseLink(license) {
-  if (license !== 'none') {
-    return `\n* [License](#license)\n`
+  if (license !== 'NONE') {
+    return `\n* [License](#license)\n`;
   }
-  return ''
+  return '';
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+
 function renderLicenseSection(license) {
-  if (license !== 'none') {
+  if (license !== 'NONE') {
     return `## License
     
-    Licensed under the ${license} license`
+    Licensed under the ${license} license`;
   }
-  return ''
+  return '';
 }
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}
@@ -53,7 +50,7 @@ ${data.tests}
 \'\'\'
 ## Questions
 ${data.email}
-${data.github}
+github.com/${data.github}
 ${renderLicenseSection(data.license)}
 `;
 }
